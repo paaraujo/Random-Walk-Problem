89db07c9da3e8a733f6c54401bd2c9e8e58d6a6c
Paulo Araujo <paulo_marques_araujo@hotmail.com>
2020-09-17 14:09:59 -0300

    Script updated

--- script.py
+++ script.py
@@ -1,31 +1,20 @@
 import numpy as np
-from math import sqrt 
 
 #initial_point = [0,0]
-def run(samples):
+def run(steps):
     # initialization
-    size = 1000000000 # 1 billion - IMPORTANT! Check your available memory
-    cycles = samples // size
-    remain = samples % size
-    # running cycles
-    finalX = finalY = 0
-    for i in range(cycles):
-        directions = np.random.rand(size,1) * 2*np.pi 
-        finalX  += np.sum(np.cos(directions))
-        finalY  += np.sum(np.sin(directions))
-    # running remain
-    directions = np.random.rand(remain,1) * 2*np.pi 
-    finalX  += np.sum(np.cos(directions))
-    finalY  += np.sum(np.sin(directions))
-    # returning euclidean distance from the origin [0,0]
-    return sqrt(pow(finalX,2) + pow(finalY,2))
+    numExp = 10000000
+    directions = np.random.rand(steps,numExp) * 2*np.pi 
+    Xs = np.sum(np.cos(directions),axis=0)
+    Ys = np.sum(np.sin(directions),axis=0)
+    dN2 = np.mean(np.square(Xs) + np.square(Ys))
+    dN_  = np.sqrt(dN2)
+    dN = np.mean(np.sqrt(np.square(Xs) + np.square(Ys)))
+    return [dN2,dN_,dN]
 
 # main loop
-N = [pow(10,10)] # 100 billions
+N = [5,10,15,20,25] # number of steps
+
 stat = np.zeros(len(N))
-for j, n in enumerate(N):
-    stat[j] = run(n)
-    
-dN = np.sqrt(N)
-print("Square(s) of N: ",dN)
-print("Simulated distance(s): ", stat)
+for i,n in enumerate(N):
+    print('N =',N[i],' [dN2, sqrt(dN2), dN] =',run(n))
